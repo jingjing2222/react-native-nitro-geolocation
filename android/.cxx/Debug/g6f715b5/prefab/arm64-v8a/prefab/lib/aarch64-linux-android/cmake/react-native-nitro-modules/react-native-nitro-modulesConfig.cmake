4621e49d024c4aea23551579b25b7ef116990c38
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-10-03 23:16:15 +0900

    feat: mise, android logic

---
+++ android/.cxx/Debug/g6f715b5/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-nitro-modules/react-native-nitro-modulesConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-nitro-modules::NitroModules)
+add_library(react-native-nitro-modules::NitroModules SHARED IMPORTED)
+set_target_properties(react-native-nitro-modules::NitroModules PROPERTIES
+    IMPORTED_LOCATION "/Users/kimhyeongjeong/Desktop/code/react-native-nitro-geolocation/example/node_modules/react-native-nitro-modules/android/build/intermediates/cxx/Debug/6o3p684e/obj/arm64-v8a/libNitroModules.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/kimhyeongjeong/Desktop/code/react-native-nitro-geolocation/example/node_modules/react-native-nitro-modules/android/build/headers/nitromodules"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
