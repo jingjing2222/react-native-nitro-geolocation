b75c9406450c8728d5e6b766f216f1377a9f66c7
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-10-05 21:46:27 +0900

    chore: make docs (rspress)

--- docs/docs/index.md
+++ docs/docs/index.md
@@ -13,7 +13,7 @@ hero:
       text: GitHub
       link: https://github.com/web-infra-dev/rspress
   image:
-    src: /rspress-icon.png
+    src: logo.png
     alt: Logo
 features:
   - title: Blazing fast build speed
--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -4,17 +4,15 @@ import { defineConfig } from 'rspress/config';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'My Site',
-  icon: '/rspress-icon.png',
-  logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
-  },
+  icon: '/logo.png',
+  logo: '/logo.png',
+  logoText: "react-native-nitro-geolocation",
   themeConfig: {
     socialLinks: [
       {
         icon: 'github',
         mode: 'link',
-        content: 'https://github.com/web-infra-dev/rspress',
+        content: 'https://github.com/jingjing2222/react-native-nitro-geolocation',
       },
     ],
   },
