8c73ae057ba3ea1dbe2ca8b220ada88496cc2228
JaMinsane <james.jh03@gmail.com>
2026-03-05 15:49:59 -0500

    regenerate Nitrogen outputs + update JNI_OnLoad

--- packages/react-native-nitro-geolocation/android/src/main/cpp/cpp-adapter.cpp
+++ packages/react-native-nitro-geolocation/android/src/main/cpp/cpp-adapter.cpp
@@ -1,6 +1,9 @@
 #include <jni.h>
+#include <fbjni/fbjni.h>
 #include "nitrogeolocationOnLoad.hpp"
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return margelo::nitro::nitrogeolocation::initialize(vm);
+  return facebook::jni::initialize(vm, [] {
+    margelo::nitro::nitrogeolocation::registerAllNatives();
+  });
 }
